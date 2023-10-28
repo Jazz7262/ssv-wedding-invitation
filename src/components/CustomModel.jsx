f8e2b84f0542a94f5d289e946619b2d4eb2057a2
Jazz <116513312+Jazz7262@users.noreply.github.com>
2023-10-28 21:44:09 +0000

    Updated from sandbox

--- src/components/CustomModel.jsx
+++ src/components/CustomModel.jsx
@@ -4,128 +4,124 @@ import $ from "jquery";
 import "../styles/CustomModel.css";
 
 function CustomModel1() {
-    function handleClick() {
-        $(".card-flower").addClass("card-flower-fade-out-animation");
+  function handleClick() {
+    $(".card-flower").addClass("card-flower-fade-out-animation");
 
-        setTimeout(() => {
-            // card zoom-out animation
-            window.scrollTo(0, 0);
-            $(".card-outer-wrapper").addClass("zoom-out-animation");
-            setTimeout(() => {
-                $("#card").css("display", "none");
-                // $("#card").fadeOut();
-                $(".navbar").css("z-index", "2");
-                $(".swing-icon").css("z-index", "2");
-                $(".animated-logo").css("z-index", "2");
-                $("body").removeAttr("style");
-                // document.getElementById("audio").play();
-                document.getElementById("timer-video").play();
+    setTimeout(() => {
+      // card zoom-out animation
+      window.scrollTo(0, 0);
+      $(".card-outer-wrapper").addClass("zoom-out-animation");
+      setTimeout(() => {
+        $("#card").css("display", "none");
+        $("#card").fadeOut();
+        $(".navbar").css("z-index", "2");
+        $(".swing-icon").css("z-index", "2");
+        $(".animated-logo").css("z-index", "2");
+        $("body").removeAttr("style");
+        document.getElementById("audio").play();
+        document.getElementById("timer-video").play();
 
-                startScroll();
-            }, 1000);
-        }, 1500);
-    }
+        startScroll();
+      }, 1000);
+    }, 1500);
+  }
 
-    // auto scroll starts when user clicks on "open" button
-    function startScroll() {
-        var length = document.querySelector("html").offsetHeight;
-        // setTimeout(() => {
-        //     $("html, body").animate(
-        //         {
-        //             scrollTop: length - 500,
-        //         },
-        //         30000,
-        //         "linear",
-        //         function () {
-        //             $("html, body").animate(
-        //                 {
-        //                     scrollTop: 0,
-        //                 },
-        //                 5000,
-        //                 function () {
-        //                     $(".toast-message").fadeOut();
-        //                     document.querySelector(
-        //                         "html"
-        //                     ).style.scrollBehavior = "smooth";
+  // auto scroll starts when user clicks on "open" button
+  function startScroll() {
+    var length = document.querySelector("html").offsetHeight;
+    setTimeout(() => {
+      $("html, body").animate(
+        {
+          scrollTop: length - 500,
+        },
+        30000,
+        "linear",
+        function () {
+          $("html, body").animate(
+            {
+              scrollTop: 0,
+            },
+            5000,
+            function () {
+              $(".toast-message").fadeOut();
+              document.querySelector("html").style.scrollBehavior = "smooth";
 
-        //                     $("html, body").stop();
-        //                     $("html, body").off("click");
-        //                 }
-        //             );
-        //         }
-        //     );
-        // }, 500);
+              $("html, body").stop();
+              $("html, body").off("click");
+            },
+          );
+        },
+      );
+    }, 500);
 
-        // stop the auto scroll when user clicks on screen
-        setTimeout(function () {
-            $("html, body").on("click", function (event) {
-                $("html, body").stop();
-                $(".toast-message").fadeOut();
-                $("html, body").off("click");
-                document.querySelector("html").style.scrollBehavior = "smooth";
-            });
-        }, 1000);
-    }
+    // stop the auto scroll when user clicks on screen
+    setTimeout(function () {
+      $("html, body").on("click", function (event) {
+        $("html, body").stop();
+        $(".toast-message").fadeOut();
+        $("html, body").off("click");
+        document.querySelector("html").style.scrollBehavior = "smooth";
+      });
+    }, 1000);
+  }
 
-    return (
-        <section id="card">
-            <div className="card-outer-wrapper">
-                <div className="card-inner-wrapper">
-                    <div data-aos="zoom-out" className="card-content">
-                        <h1 className="card-title m-0">
-                            Turrumturri Family's Wedding Ceremony Invitation
-                        </h1>
-                        <img
-                            src="../assets/images/line-divider.png"
-                            alt="line_divider"
-                            className="line-divider mb-4"
-                        />
-                        <h4 className="from">From:</h4>
-                        <h4 className="invitor-name mb-4">
-                            Mr. & Mrs. T. Basha
-                        </h4>
-                        <button
-                            className="btn btn-primary open-button"
-                            onClick={handleClick}
-                        >
-                            Open
-                        </button>
-                        <img
-                            src="../assets/images/card-flower.png"
-                            alt="card_flower"
-                            className="card-flower right"
-                        />
-                        <img
-                            src="../assets/images/card-flower.png"
-                            alt="card_flower"
-                            className="card-flower left"
-                        />
-                    </div>
+  return (
+    <section id="card">
+      <div className="card-outer-wrapper">
+        <div className="card-inner-wrapper">
+          <div data-aos="zoom-out" className="card-content">
+            <h1 className="card-title m-0">
+              Turrumturri Family's Wedding Ceremony Invitation
+            </h1>
+            <img
+              src="../assets/images/line-divider.png"
+              alt="line_divider"
+              className="line-divider mb-4"
+            />
+            <h4 className="from">From:</h4>
+            <h4 className="invitor-name mb-4">Mr. & Mrs. T. Basha</h4>
+            <button
+              className="btn btn-primary open-button"
+              onClick={handleClick}
+            >
+              Open
+            </button>
+            <img
+              src="../assets/images/card-flower.png"
+              alt="card_flower"
+              className="card-flower right"
+            />
+            <img
+              src="../assets/images/card-flower.png"
+              alt="card_flower"
+              className="card-flower left"
+            />
+          </div>
 
-                    <img
-                        src="../assets/images/card-corner.png"
-                        alt="card_corner"
-                        className="card-corner top-right"
-                    />
-                    <img
-                        src="../assets/images/card-corner.png"
-                        alt="card_corner"
-                        className="card-corner top-left"
-                    />
-                    <img
-                        src="../assets/images/card-corner.png"
-                        alt="card_corner"
-                        className="card-corner bottom-right"
-                    />
-                    <img
-                        src="../assets/images/card-corner.png"
-                        alt="card_corner"
-                        className="card-corner bottom-left"
-                    />
-                </div>
-            </div>
-        </section>
-    );
+          <img
+            src="../assets/images/card-corner.png"
+            alt="card_corner"
+            className="card-corner top-right"
+          />
+          <img
+            src="../assets/images/card-corner.png"
+            alt="card_corner"
+            className="card-corner top-left"
+          />
+          <img
+            src="../assets/images/card-corner.png"
+            alt="card_corner"
+            className="card-corner bottom-right"
+          />
+          <img
+            src="../assets/images/card-corner.png"
+            alt="card_corner"
+            className="card-corner bottom-left"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default CustomModel1;
